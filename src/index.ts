3e642143d86bd2da616c38649330a2fc5f17bd37
Douglas G. Moore <doug@dglmoore.com>
2019-06-25 14:31:22 -0700

    Support number[], Int32Array and ArrayBuffer timeseries

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 const informcpp = require('../build/Release/informcpp');
 
-export function mutualInfo(xs: number[], ys: number[]): number {
+export type Series = number[] | Int32Array | ArrayBuffer;
+
+export function mutualInfo(xs: Series, ys: Series): number {
     return informcpp.mutualInfo(xs, ys);
 }
--- test/mutual_info.test.ts
+++ test/mutual_info.test.ts
@@ -10,36 +10,53 @@ describe('mutual information', () => {
         expect(() => mutualInfo([], [])).toThrow(/NULL/);
     });
 
-    test('.invalid element', () => {
-        expect(() => mutualInfo([-1, 0, 0], [1, 1, 0])).toThrow(/negative/);
-        expect(() => mutualInfo([1, 0, 0], [-1, 1, 0])).toThrow(/negative/);
-        expect(() => mutualInfo([1, 0, 0], [-1, 1, 0])).toThrow(/negative/);
+    test.each`
+        xs                                   | ys                                   | message
+        ${[-1, 0, 0]}                        | ${[1, 1, 0]}                         | ${'negative'}
+        ${[1, 0, 0]}                         | ${[-1, 1, 0]}                        | ${'negative'}
+        ${new Int32Array([-1, 0, 0])}        | ${new Int32Array([1, 1, 0])}         | ${'negative'}
+        ${new Int32Array([1, 0, 0])}         | ${new Int32Array([-1, 1, 0])}        | ${'negative'}
+        ${new Int32Array([-1, 0, 0]).buffer} | ${new Int32Array([1, 1, 0]).buffer}  | ${'negative'}
+        ${new Int32Array([1, 0, 0]).buffer}  | ${new Int32Array([-1, 1, 0]).buffer} | ${'negative'}
+        ${[0.5, 0, 0]}                       | ${[1, 1, 0]}                         | ${'not an integer'}
+        ${[0, 0, 0]}                         | ${[0.5, 1, 0]}                       | ${'not an integer'}
+    `('.invalid element', ({ xs, ys, message }) => {
+        expect(() => mutualInfo(xs, ys)).toThrow(message);
     });
 
-    test('.can', () => {
-        expect(mutualInfo([0, 0, 0, 0, 1, 1, 1, 1], [1, 1, 1, 1, 0, 0, 0, 0])).toBeCloseTo(1.0, 6);
+    test('.invalid buffer length', () => {
+        const xs = new Int8Array([0, 0, 0]).buffer;
+        const ys = new Int8Array([1, 0, 1]).buffer;
+        expect(() => mutualInfo(xs, ys)).toThrow(/inconsistent/);
+    });
 
-        expect(mutualInfo([0, 0, 1, 1, 1, 1, 0, 0, 0], [1, 1, 0, 0, 0, 0, 1, 1, 1])).toBeCloseTo(0.991076, 6);
+    test.each`
+        xs                                      | ys                                      | mi
+        ${[0, 0, 0, 0, 1, 1, 1, 1]}             | ${[1, 1, 1, 1, 0, 0, 0, 0]}             | ${1.0}
+        ${[0, 1, 0, 1, 0, 1, 0, 1]}             | ${[0, 2, 0, 2, 0, 2, 0, 2]}             | ${1.0}
+        ${[1, 0, 0, 1, 0, 0, 1, 0]}             | ${[2, 0, 1, 2, 0, 1, 2, 0]}             | ${0.954434}
+        ${[0, 0, 1, 1, 1, 1, 0, 0, 0]}          | ${[1, 1, 0, 0, 0, 0, 1, 1, 1]}          | ${0.991076}
+        ${[1, 1, 0, 1, 0, 1, 1, 1, 0]}          | ${[1, 1, 0, 0, 0, 1, 0, 1, 1]}          | ${0.07278}
+        ${[0, 0, 0, 0, 0, 0, 0, 0, 0]}          | ${[1, 1, 1, 0, 0, 0, 1, 1, 1]}          | ${0.0}
+        ${[1, 1, 1, 1, 0, 0, 0, 0, 1]}          | ${[1, 1, 1, 0, 0, 0, 1, 1, 1]}          | ${0.07278}
+        ${[1, 1, 0, 0, 1, 1, 0, 0, 1]}          | ${[1, 1, 1, 0, 0, 0, 1, 1, 1]}          | ${0.018311}
+        ${[0, 0, 1, 1, 2, 1, 1, 0, 0]}          | ${[0, 0, 0, 1, 1, 1, 0, 0, 0]}          | ${0.473851}
+        ${[0, 1, 0, 0, 1, 0, 0, 1, 0]}          | ${[1, 0, 0, 1, 0, 0, 1, 0, 0]}          | ${0.251629}
+        ${[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]} | ${[0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2]} | ${0.666667}
+    `('.can', ({ xs, ys, mi }) => {
+        const xsInt32 = new Int32Array(xs);
+        const ysInt32 = new Int32Array(ys);
+        const xsBuffer = xsInt32.buffer;
+        const ysBuffer = ysInt32.buffer;
 
-        expect(mutualInfo([1, 1, 0, 1, 0, 1, 1, 1, 0], [1, 1, 0, 0, 0, 1, 0, 1, 1])).toBeCloseTo(0.07278, 6);
-
-        expect(mutualInfo([0, 0, 0, 0, 0, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0, 1, 1, 1])).toBeCloseTo(0, 6);
-
-        expect(mutualInfo([1, 1, 1, 1, 0, 0, 0, 0, 1], [1, 1, 1, 0, 0, 0, 1, 1, 1])).toBeCloseTo(0.07278, 6);
-
-        expect(mutualInfo([1, 1, 0, 0, 1, 1, 0, 0, 1], [1, 1, 1, 0, 0, 0, 1, 1, 1])).toBeCloseTo(0.018311, 6);
-
-        expect(mutualInfo([0, 1, 0, 1, 0, 1, 0, 1], [0, 2, 0, 2, 0, 2, 0, 2])).toBeCloseTo(1.0, 6);
-
-        expect(mutualInfo([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1], [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2])).toBeCloseTo(
-            0.666667,
-            6,
-        );
-
-        expect(mutualInfo([0, 0, 1, 1, 2, 1, 1, 0, 0], [0, 0, 0, 1, 1, 1, 0, 0, 0])).toBeCloseTo(0.473851, 6);
-
-        expect(mutualInfo([0, 1, 0, 0, 1, 0, 0, 1, 0], [1, 0, 0, 1, 0, 0, 1, 0, 0])).toBeCloseTo(0.251629, 6);
-
-        expect(mutualInfo([1, 0, 0, 1, 0, 0, 1, 0], [2, 0, 1, 2, 0, 1, 2, 0])).toBeCloseTo(0.954434, 6);
+        expect(mutualInfo(xs, ys)).toBeCloseTo(mi, 6);
+        expect(mutualInfo(xs, ysInt32)).toBeCloseTo(mi, 6);
+        expect(mutualInfo(xs, ysBuffer)).toBeCloseTo(mi, 6);
+        expect(mutualInfo(xsInt32, ys)).toBeCloseTo(mi, 6);
+        expect(mutualInfo(xsInt32, ysInt32)).toBeCloseTo(mi, 6);
+        expect(mutualInfo(xsInt32, ysBuffer)).toBeCloseTo(mi, 6);
+        expect(mutualInfo(xsBuffer, ys)).toBeCloseTo(mi, 6);
+        expect(mutualInfo(xsBuffer, ysInt32)).toBeCloseTo(mi, 6);
+        expect(mutualInfo(xsBuffer, ysBuffer)).toBeCloseTo(mi, 6);
     });
 });
